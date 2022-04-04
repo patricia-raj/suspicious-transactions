eb93610b787d28c4065e1c82ca98934aa3166ac0
“Patricia-r” <99471693+patricia-raj@users.noreply.github.com>
2022-04-04 06:32:32 -0500

    Added queries

--- query.sql
+++ query.sql
@@ -15,16 +15,49 @@ SELECT *
 FROM transaction;
 
 
+-- Group transactions by Card holder
+SELECT cardholder_name, transaction.*
+FROM transaction 
+LEFT JOIN  credit_card ON credit_card.creditcard_no= transaction.credit_card
+LEFT JOIN card_holder ON card_holder.cardholder_id = credit_card.cardholder_id;
 
+SELECT credit_card, COUNT(*)
+FROM transaction
+GROUP BY credit_card;
 
 -- Query transaction less than $2.0
-SELECT *
+SELECT count(*)
 FROM transaction
 WHERE amount < 2.0;
 
 
--- Group transaction by credit card and transaction amount less than $2.0
-SELECT credit_card, count(*)
+SELECT count(*)
+FROM transaction 
+LEFT JOIN  credit_card ON credit_card.creditcard_no= transaction.credit_card
+LEFT JOIN card_holder ON card_holder.cardholder_id = credit_card.cardholder_id
+WHERE amount < 2.0;
+
+
+-- Transactions by credit card with charges less than $2.0
+SELECT credit_card, COUNT(*)
 FROM transaction
 WHERE amount < 2.0
 GROUP BY credit_card;
+
+
+--  Transactions grouped by card holder with the count of transactions charged less than $2.0
+SELECT cardholder_name, COUNT(*)
+FROM transaction 
+LEFT JOIN credit_card ON credit_card.creditcard_no= transaction.credit_card
+LEFT JOIN card_holder ON card_holder.cardholder_id = credit_card.cardholder_id
+WHERE amount < 2.0
+GROUP BY  cardholder_name;
+
+
+SELECT cardholder_name, transaction.*
+FROM transaction 
+left JOIN  credit_card ON credit_card.creditcard_no= transaction.credit_card
+left JOIN card_holder ON card_holder.cardholder_id = credit_card.cardholder_id
+WHERE amount < 2.0
+and extract(hour from transaction_date) >= 7 and extract(hour from transaction_date) <= 9;
+
